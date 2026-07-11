7076b28bb93d1b98a2ef836daae57f5cdae51cd1
AydarGataullin7 <aydarcr7@gmail.com>
2026-07-11 16:46:32 +0500

    Calculate and display total order price

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.decorators import user_passes_test
 
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import views as auth_views
+from django.db.models import Sum, F
 
 
 from foodcartapp.models import Product, Restaurant, Order
@@ -94,5 +95,6 @@ def view_restaurants(request):
 
 @user_passes_test(is_manager, login_url='restaurateur:login')
 def view_orders(request):
-    orders = Order.objects.all()
+    orders = Order.objects.annotate(total_price=Sum(
+        F('items__product__price')*F('items__quantity')))
     return render(request, template_name='order_items.html', context={'orders': orders})
