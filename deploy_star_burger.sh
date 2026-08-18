3b5fa34c80c95a684504cb4613badf02361e1071
AydarGataullin7 <aydarcr7@bk.ru>
2026-08-18 04:16:29 +0000

    fix deploy script

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -18,7 +18,7 @@ echo "Installing Node.js dependencies..."
 npm install
 
 echo "Building frontend..."
-./node_modules/.bin/parcel build bundle-src/index.js --dist-dir bundles --public-url="./"CC
+./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
 
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
